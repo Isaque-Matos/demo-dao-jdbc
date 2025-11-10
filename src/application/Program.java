816b2442fdd4ab34c3bf1add29db7cc00a2e8fd4
Isaque Matos <isaque.smatos@hotmail.com>
2025-11-10 16:09:54 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,11 @@
-package application;
-
+package application;import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
+
 
 public class Program {
 
@@ -29,6 +29,11 @@ public class Program {
 		for(Seller sel : list) {
 			System.out.println(sel);
 		}
+		
+		System.out.println("\n=== TEST 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
