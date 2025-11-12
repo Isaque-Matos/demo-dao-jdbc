e20b8489d9895ecc1d3e28a93dc15fdd6ea25310
Isaque Matos <isaque.smatos@hotmail.com>
2025-11-12 14:12:35 -0300

    department findAll implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,35 +1,38 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
-
 public class Program2 {
 
 	public static void main(String[] args) {
-	
+
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-		
+
 		System.out.println("=== TEST 1: department findById ===");
 		Department dep = departmentDao.findById(3);
 		System.out.println(dep);
+
 		
-		/*
-		 * System.out.println("=== TEST 2: department insert ==="); Department
-		 * newDepartment = new Department(5,"D1"); departmentDao.insert(newDepartment);
-		 * System.out.println("\nInserted! New id = " + newDepartment.getId());
-		 */
-		
-		System.out.println("=== TEST 3: department delete ===");
-		departmentDao.deleteById(6);
-		System.out.println("ID deleted!");
-		
-		System.out.println("=== TEST 4: department update ===");
-		dep = departmentDao.findById(5);
-		dep.setName("D2");
-		departmentDao.update(dep);
-		System.out.println("Update completed");
+		 System.out.println("=== TEST 2: department insert ==="); Department
+		 newDepartment = new Department(5,"D1"); departmentDao.insert(newDepartment);
+		 System.out.println("\nInserted! New id = " + newDepartment.getId());
+		  
+		 System.out.println("=== TEST 3: department delete ===");
+		 departmentDao.deleteById(6); System.out.println("ID deleted!");
+		  
+		 System.out.println("=== TEST 4: department update ==="); dep =
+		 departmentDao.findById(5); dep.setName("D2"); departmentDao.update(dep);
+		 System.out.println("Update completed");
+		  
+		 System.out.println("=== TEST 5: department findAll ===");
+		 List<Department> list = departmentDao.findAll();
+		 for (Department d : list) {
+			System.out.println(d);
+		}
 	}
 
 }
